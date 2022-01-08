29d3d08f2315c9bc514606bfb676d94e1dd69030
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2022-01-08 16:01:37 -0300

    Adição da logo principal

---
+++ src/components/MainLogo/index.tsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+import logoImg from "../../img/logo-cosmo-alpha.webp";
+
+function MainLogo() {
+  return (
+    <img className="main-logo" src={logoImg} alt="Logo da Cosmo Mapeamento" />
+  );
+}
+
+export default MainLogo;
