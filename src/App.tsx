4bdb65f24b8f43cc3307ed72ca3ca3ca1c268beb
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2022-01-08 16:03:39 -0300

    Arquivo principal da aplicação (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import "./App.scss";
+import MainLogo from "./components/MainLogo";
+import MainBg from "./components/MainBg";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import ServicesSection from "./components/main-sections/ServicesSection";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MainBg />
+
+      <header>
+        <MainLogo />
+        <Navbar />
+      </header>
+
+      <main>
+        <Title color="white">
+          <span className="smaller">DESCUBRA O UNIVERSO DO</span>
+          <strong>MAPEAMENTO AÉREO</strong>
+        </Title>
+        <ServicesSection />
+      </main>
+    </>
+  );
 }
 
 export default App;
