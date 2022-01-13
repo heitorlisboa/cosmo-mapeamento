fe08572354c6f830921146a877c807e28c96ecad
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2022-01-12 21:46:25 -0300

    Implementação de classes opcionais

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,11 +3,16 @@ import "./style.scss";
 
 interface TitleProps {
   color: "blurple" | "white";
+  className?: string;
 }
 
 const Title: FC<TitleProps> = (props) => {
   return (
-    <h2 className={`secondary-title secondary-title--${props.color}`}>
+    <h2
+      className={`secondary-title secondary-title--${props.color} ${
+        props.className || ""
+      }`}
+    >
       {props.children}
     </h2>
   );
