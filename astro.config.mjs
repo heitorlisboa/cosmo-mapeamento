70607682f4241e4e349076fc9a2d7813b17c9ac5
heitorlisboa <heitor01101000@gmail.com>
2023-01-08 18:28:18 -0300

    chore: add `site` to Astro's config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    integrations: [tailwind(), react()],
+    site: 'https://www.cosmomapeamento.com.br',
+  }
+);
